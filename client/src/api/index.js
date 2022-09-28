2d81fb731e3e1d3346291f17ff4e9623e6307662
IamTarunG <tarungudipalli5@gmail.com>
2022-09-28 09:21:22 +0530

    Changed URL

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://poststravel.onrender.com" });
 
 export const getPost = () => API.get("/posts");
 export const createPost = (formData) => API.post("/posts", formData);
